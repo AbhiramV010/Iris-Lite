be30dd61244ad7d93ae60d8c636e36b436bdb13e
Abhiram Vadali <abhiram.vadali@gmail.com>
2026-02-15 13:11:40 -0500

    attempted to refine grass detection
    It's going mental

--- zoneMonitor.py
+++ zoneMonitor.py
@@ -11,21 +11,28 @@ def startCam():
     ret, frame = cam.read()
     if not ret: return
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    grassRange = cv2.inRange(hsv, np.array([30, 45, 45]), np.array([95, 255, 255]))
+    grassRange = cv2.inRange(hsv, np.array([35, 40, 40]), np.array([85, 255, 255]))    
 
 def defineZone(mask):
-    kernel = np.ones((9,9), np.uint8) # optimal matrice dimensions determined using a pixel ruler and sample image 
-
-    mask=cv2.dilate(mask,kernel,iterations=2)
-    mask=cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel) # NOTE: do not make the kernel bigger, use iteration with the same kernel 
-    mask=cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    kernel = np.ones((3,3), np.uint8)
+    
+    mask=cv2.dilate(mask,kernel,iterations=1)
+    mask = cv2.erode(mask, kernel, iterations=2)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
+    mask = cv2.dilate(mask, kernel, iterations=2)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=4)
     
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if not contours: return None
-    field = max(contours, key=cv2.contourArea)
-    epsilon = 0.02 * cv2.arcLength(field, True)
+
+    min_area = (frame.shape[0] * frame.shape[1]) * 0.05
+    valid_contours = [c for c in contours if cv2.contourArea(c) > min_area] # filter out any green patches that take up less than 5% of screen space 
+
+    if not valid_contours: return None
+
+    field = max(valid_contours, key=cv2.contourArea)
+    epsilon = 0.02 * cv2.arcLength(field, True) 
     approx = cv2.approxPolyDP(field, epsilon, True)
-    cv2.imshow("hsvCam",hsv)
 
     return approx
 
@@ -36,7 +43,6 @@ def perFrameGrass():
     # needs to be moving because people run on grass, bike bikes on grass, but a stationary tree doesn't need to be counted, ts supposed to be there
     pass 
 
-
 startCam()
 static_grass_zone = defineZone(grassRange)
 
@@ -44,9 +50,9 @@ while True:
     ret, frame = cam.read()
     if static_grass_zone is not None: 
         overlay = frame.copy()
-        cv2.drawContours(overlay, [static_grass_zone], -1, (220, 220, 220), thickness=-1)
+        cv2.drawContours(overlay, [static_grass_zone], -1, (220, 120, 120), thickness=-1)
         cv2.addWeighted(overlay, 0.5, frame, 0.5, 0, frame)
-        cv2.drawContours(frame, [static_grass_zone], -1, (255, 255, 255), thickness=2)  
+        cv2.drawContours(frame, [static_grass_zone], -1, (255, 155, 155), thickness=2)  
     if not ret: break
 
     cv2.imshow("steamic26-cam", frame)
