2d3da765174e9b7b816a1b54ab537485dd2cc7f8
Abhiram Vadali <abhiram.vadali@gmail.com>
2026-02-15 11:06:09 -0500

    Refactor zoneMonitor.py for background subtraction

--- zoneMonitor.py
+++ zoneMonitor.py
@@ -4,25 +4,25 @@ import numpy as np
 def startCam():
     global cam, ret, frame, hsv, grassRange, fgbg
 
-    # fgbg = cv2.bgsegm.createBackgroundSubtractorCNT() # algorithm that already exists, using standard deviation to identify changes
+    fgbg = cv2.createBackgroundSubtractorKNN() # algorithm that already exists, using standard deviation to identify changes
     cam = cv2.VideoCapture(0)
-    
-    for _ in range(30): cam.read() 
+    for _ in range(60): cam.read() 
     
     ret, frame = cam.read()
     if not ret: return
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    grassRange = cv2.inRange(hsv, np.array([30, 70, 20]), np.array([90, 255, 255]))
+    grassRange = cv2.inRange(hsv, np.array([30, 45, 45]), np.array([95, 255, 255]))
 
 def defineZone(mask):
     kernel = np.ones((9,9), np.uint8) # optimal matrice dimensions determined using a pixel ruler and sample image 
 
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel) # NOTE: do not make the kernel bigger, use iteration with the same kernel 
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    mask=cv2.dilate(mask,kernel,iterations=2)
+    mask=cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel) # NOTE: do not make the kernel bigger, use iteration with the same kernel 
+    mask=cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if not contours: return None
-    field = max(contours, key=cv2.contourArea) # simplify object from 1000s of vertices to far less
+    field = max(contours, key=cv2.contourArea)
     epsilon = 0.02 * cv2.arcLength(field, True)
     approx = cv2.approxPolyDP(field, epsilon, True)
     cv2.imshow("hsvCam",hsv)
@@ -30,7 +30,7 @@ def defineZone(mask):
     return approx
 
 def perFrameGrass(): 
-    # TODO:
+    # TODO: 
     # check the presence of an moving object on the grass
         # bottom-most part of moving object MUST be on the grass, if not just disregard
     # needs to be moving because people run on grass, bike bikes on grass, but a stationary tree doesn't need to be counted, ts supposed to be there
