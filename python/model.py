2526624a88c2a6c8858bf7e7a011dedddf7ba79a
Abhiram <abhiram.vadali@gmail.com>
2026-03-23 09:16:20 -0400

    made layer 2 of sound code

---
+++ python/model.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+
+def defineModel():
+    model = "will be defined later"
+    return model 
--- python/sound_monitor.py
+++ python/sound_monitor.py
@@ -1,22 +1,63 @@
-import tensorflow as tf
-import pyaudio
 import numpy as np
+import pyaudio
+import collections
+import tensorflow.lite as tflite
 import librosa
+from captureinfo import CaptureClass 
+
+SOC = [0,1,2,3,4,5,6,7] # sounds of concern, have a look below
+# 0 -> genuine silence
+# 1 -> ambience (source: from an MPV suburban front-porch) 
+# 2 -> car crashing
+# 3 -> car screeching/skidding away
+# 4 -> screaming
+# 5 -> gunshots
+# 6 -> glass breaking 
+# 7 -> door banging/punching/aggressive-knocking/kicking 
+
+MODEL = "model.tflite"
+RATE = 16000 
+CHUNK = 4096 # change to 1024 if poor perf, it'll eat resources tho 
+THRESH = 0.03  
+
+interpreter = tflite.Interpreter(model_path=MODEL)
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paFloat32, channels=1, rate=16000, input=True, frames_per_buffer=1024, input_device_index=1)
+stream = p.open(format=pyaudio.paFloat32, channels=1, rate=RATE,
+                input=True, frames_per_buffer=CHUNK)
 
-def preProcessor(audio):
-    print("processed")
-    mel_spec = librosa.feature.melspectrogram(y=audio, sr=16000, n_mels=128)
-    log_mel_spec = librosa.power_to_db(mel_spec, ref=np.max) # short time fourier transform
-    return log_mel_spec
+audio_buffer = collections.deque(maxlen=RATE * 3)
 
-stream.start_stream()
+def pre_process(audio_np):
+    spec = librosa.feature.melspectrogram(y=audio_np, sr=RATE, n_mels=128)
+    log_spec = librosa.power_to_db(spec, ref=np.max)
+    return log_spec[:, :98] 
 
-while True:
-    data = stream.read(2048, exception_on_overflow=False)
-    audio_data = np.frombuffer(data, dtype=np.float32).reshape(-1, 2)
-    mono_data = audio_data[:, 0]
-    
-    processedAudio = preProcessor(mono_data)
+try:
+    while True:
+        data = stream.read(CHUNK, exception_on_overflow=False)
+        chunk = np.frombuffer(data, dtype=np.float32)
+        audio_buffer.extend(chunk)
+
+        rms = np.sqrt(np.mean(chunk**2))
+        
+        # is the noise actually a big deal & do we have more than two seconds of audio
+        # if it doesn't meet BOTH (not one) conditions, it'll skip over ALL processing, giving massive saves
+        if rms > THRESH and len(audio_buffer) >= (RATE * 2):
+            processed_data = pre_process(np.array(audio_buffer))
+            input_data = processed_data[np.newaxis, ..., np.newaxis].astype(np.float32)
+            interpreter.set_tensor(input_details[0]['index'], input_data)
+            interpreter.invoke()
+            output_data = interpreter.get_tensor(output_details[0]['index'])
+            prediction = np.argmax(output_data)
+            confidence = output_data[0][prediction]
+            if prediction in SOC and confidence > 0.8: # is the prediction in the "knowledge" and is it confident enough
+                print(f"The sound was {prediction}, reported with {confidence:.2f}% confidence")
+
+except KeyboardInterrupt: 
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
