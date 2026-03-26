2eda6aa158962203a5c926775e9be8b3a4338173
Abhiram <abhiram.vadali@gmail.com>
2026-03-25 20:45:42 -0400

    made detector_pipeline.py

---
+++ clips/detector_pipeline.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+# init
+
+# Tier 1 -> Basics, like motion anomalies, camera disturbance, and 
+
+# Tier 2 -> More heavy stuff: motion entropy (which is basically predictability),   
