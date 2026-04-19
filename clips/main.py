5a15ba8e2b250a4037178b0703175faa63f0f7fa
24k-zilxh <abhiram.vadali@gmail.com>
2026-04-19 14:48:09 -0400

    added debug labels

--- clips/main.py
+++ clips/main.py
@@ -43,10 +43,13 @@ def save_clip_worker(trigger_name, capture_class: CaptureClass):
     print(f"start: {capture_class.startTime} | end: {capture_class.endTime}")
     if capture_class.isMotionSensor == True:
         cmd = f"ffmpeg -y -framerate {FPS} -i {tmp}/%05d.jpg -c:v libx264 -preset ultrafast -crf 28 -pix_fmt yuv420p {SSD_PATH}/{trigger_name}_{ts}_mthn.mp4"
+        print("clip taken") 
     elif capture_class.isDoorSensor == True:
         cmd = f"ffmpeg -y -framerate {FPS} -i {tmp}/%05d.jpg -c:v libx264 -preset ultrafast -crf 28 -pix_fmt yuv420p {SSD_PATH}/{trigger_name}_{ts}_drsn.mp4"
+        print("clip taken") 
     else: 
-        cmd = f"ffmpeg -y -framerate {FPS} -i {tmp}/%05d.jpg -c:v libx264 -preset ultrafast -crf 28 -pix_fmt yuv420p {SSD_PATH}/{trigger_name}_{ts}.mp4" 
+        cmd = f"ffmpeg -y -framerate {FPS} -i {tmp}/%05d.jpg -c:v libx264 -preset ultrafast -crf 28 -pix_fmt yuv420p {SSD_PATH}/{trigger_name}_{ts}.mp4"
+        print("clip taken") 
     
     subprocess.run(cmd.split(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
     shutil.rmtree(tmp) 
