8b0952436ee59ba2aa54378732ffeb07123c13c5
Abhiram <abhiram.vadali@gmail.com>
2026-04-19 16:24:57 -0400

    made shell file

--- IRIS_Lite.sh
+++ IRIS_Lite.sh
@@ -1,47 +1,20 @@
-#!/bin/bash
+echo Starting the Iris_Lite camera system
 
-CYAN='\033[0;36m'
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m'
+python /clips/startCamera.py
+echo Camera util started
 
-draw_progress_bar() {
-    local duration=$1
-    local width=40
-    for ((progress=0; progress<=width; progress++)); do
-        let "percent = progress * 100 / width"
-        printf "\r${CYAN}Initializing IRIS-Lite [${GREEN}"
-        for ((i=0; i<progress; i++)); do printf "━"; done
-        for ((i=progress; i<width; i++)); do printf " "; done
-        printf "${CYAN}] %d%%${NC}" $percent
-        sleep $(echo "$duration / $width" | bc -l)
-    done
-    echo -e "\n"
-}
+python /clips/main.py
+echo Main util started
 
-if [ ! -f "./build/PerceptualCompressor" ]; then
-    echo -e "${CYAN}Binary not found. Compiling PerceptualCompressor...${NC}"
-    mkdir -p build
-    cd build || exit
-    cmake .. && make -j$(nproc)
-    
-    if [ $? -eq 0 ]; then
-        echo -e "${GREEN}Build successful.${NC}"
-        cd ..
-    else
-        echo -e "${RED}Build failed! Check your CMakeLists.txt and dependencies.${NC}"
-        exit 1
-    fi
-fi
+python /clips/detector_pipeline.py 
+echo Detector Pipeline started
 
-python3 clips/listener.py > listener.log 2>&1 &
-python3 clips/detector_pipeline.py > pipeline.log 2>&1 &
-python3 clips/zone_monitor.py > zone.log 2>&1 &
-python3 clips/main.py > main_py.log 2>&1 &
+python /clips/listener.py
+echo Listener tool started
 
-draw_progress_bar 1.5
+python /clips/zone_monitor.py
+echo Zone monitor started
 
-echo -e "${GREEN}Clip Software Started${NC}"
-./build/PerceptualCompressor
+g++ /Compression/src/main.cpp
 
-trap "kill 0" EXIT
+echo All systems started
