abbddede682f7bf6a596a1b29f804d1027eb86f2
24k-zilxh <abhiram.vadali@gmail.com>
2026-04-19 17:51:49 -0400

    .

--- IRIS_Lite.sh
+++ IRIS_Lite.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 echo Starting the Iris_Lite camera system.
 
+sleep 1
 uxterm -T "Camera Utility" -e "python3 ./clips/startCamera.py" &
+
+sleep 1
 uxterm -T "Main Utility" -e "python3 ./clips/main.py" &
+
+sleep 1
 uxterm -T "Detector Pipeline" -e "python3 ./clips/detector_pipeline.py" &
+
+sleep 1
 uxterm -T "Listener Tool" -e "python3 ./clips/listener.py" &
+
+sleep 1
 uxterm -T "Zone Monitor" -e "python3 ./clips/zone_monitor.py" &
 
+sleep 1
 uxterm -T "Compression System" -e "sh -c 'g++ ./Compression/src/main.cpp -o ./Compression/src/compressor && ./Compression/src/compressor; exec bash'" &
 
+sleep 1
 top
 
 echo All systems started.
