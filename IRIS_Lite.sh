ddd2a218b88b5df52dd3375e8ea84e2d9942c0cd
Abhiram <abhiram.vadali@gmail.com>
2026-04-19 16:32:30 -0400

    shellfile made

--- IRIS_Lite.sh
+++ IRIS_Lite.sh
@@ -1,20 +1,20 @@
 echo Starting the Iris_Lite camera system.
 
-python /clips/startCamera.py
+python ./clips/startCamera.py
 echo Camera util started
 
-python /clips/main.py
+python ./clips/main.py
 echo Main util started
 
-python /clips/detector_pipeline.py 
+python ./clips/detector_pipeline.py 
 echo Detector Pipeline started
 
-python /clips/listener.py
+python ./clips/listener.py
 echo Listener tool started
 
-python /clips/zone_monitor.py
+python ./clips/zone_monitor.py
 echo Zone monitor started
 
-g++ /Compression/src/main.cpp
+g++ ./Compression/src/main.cpp
 
 echo All systems started
