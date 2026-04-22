3ad3c8ce39bf2b9233078fa2e2a736e4837f9ab1
Abhiram <abhiram.vadali@gmail.com>
2026-04-21 20:49:34 -0400

    debug

--- clips/main.py
+++ clips/main.py
@@ -23,9 +23,11 @@ CONN = LISTENER.accept()
 def lock_memory():
     try:
         ctypes.CDLL("libc.so.6").mlockall(1 | 2)
+        print("memory locked")
     except Exception: pass
 
 buffer_lock = threading.Lock()
+print("started cam")
 
 if __name__ == "__main__":
     lock_memory()
