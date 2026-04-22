425c46bb54dd7155a79197a1facfe9cf30721e89
Abhiram <abhiram.vadali@gmail.com>
2026-04-21 20:45:38 -0400

    fixed main.py

--- clips/main.py
+++ clips/main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from multiprocessing import shared_memory
+from multiprocessing.connection import Listener
 import threading
 import time
 import os
@@ -12,6 +13,11 @@ BUFFER_MINUTES = 5
 FRAME_BUFFER_SIZE = FPS * 60 * BUFFER_MINUTES 
 SLOT_SIZE = 80000
 SHM_NAME = "iris_live_frame"
+INDICE_SHM = "iris_frame_indices"
+ADDRESS = ('127.0.0.1', 8989)
+global LISTENER, CONN
+LISTENER = Listener(ADDRESS, authkey=b'1000011')
+CONN = LISTENER.accept();
 
 def lock_memory():
     try:
@@ -20,12 +26,18 @@ def lock_memory():
 
 buffer_lock = threading.Lock()
 
+def checkAlert(): # checks if alert happened, then writes to frame_indices shm
+    with conn.
+    
+
 if __name__ == "__main__":
     lock_memory()
     try:
         shm = shared_memory.SharedMemory(name=SHM_NAME)
+        frame_indices = shared_memory.SharedMemory(name=INDICE_SHM, size=16) # 16 bytes, for two uint64 vars in c++
         stream_view = np.ndarray((1080, 1920, 3), dtype=np.uint8, buffer=shm.buf)
     except FileNotFoundError: sys.exit(1)
+
 
     shm_names = ["iris_frame_buffer_data", "iris_frame_sizes", "iris_frame_head_tail"]
     sizes = [FRAME_BUFFER_SIZE * SLOT_SIZE, FRAME_BUFFER_SIZE * 4, 16]
@@ -40,6 +52,8 @@ if __name__ == "__main__":
     head_tail = np.ndarray((2,), dtype=np.uint64, buffer=shms[2].buf)
     head_tail[0] = head_tail[1] = 0
 
+
+    ctr=0 # counter for frame indices, VERY important
     while True:
         t_start = time.time()
         _, compressed = cv2.imencode('.jpg', stream_view, [cv2.IMWRITE_JPEG_QUALITY, 25])
