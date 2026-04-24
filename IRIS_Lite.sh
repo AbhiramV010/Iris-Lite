7332f4e956662d36ef5ce05d66eb405aad4d55f9
Abhiram <abhiram.vadali@gmail.com>
2026-04-24 12:01:42 -0400

    made fixes

--- IRIS_Lite.sh
+++ IRIS_Lite.sh
@@ -14,22 +14,22 @@ echo "Starting Iris Lite"
 draw_bar 0
 
 uxterm -T "Camera Utility" -e "python3 ./Decision/startCamera.py" & 
-sleep 1 && draw_bar 1
+sleep 2 && draw_bar 1
 
 uxterm -T "Main Utility" -e "python3 ./Decision/main.py" & 
-sleep 1 && draw_bar 2
+sleep 2 && draw_bar 2
 
 uxterm -T "Detector Pipeline" -e "python3 ./Decision/detector_pipeline.py" & 
-sleep 1 && draw_bar 3
+sleep 2 && draw_bar 3
 
 uxterm -T "Listener Tool" -e "python3 ./Decision/listener.py" & 
-sleep 1 && draw_bar 4
+sleep 2 && draw_bar 4
 
 uxterm -T "Zone Monitor" -e "python3 ./Decision/zone_monitor.py" & 
-sleep 1 && draw_bar 5
+sleep 2 && draw_bar 5
 
 uxterm -T "Compression System" -e "sh -c 'g++ ./Compression/src/main.cpp -o ./Compression/src/compressor && ./Compression/src/compressor; exec bash'" &
-sleep 1 && draw_bar 6
+sleep 2 && draw_bar 6
 
 echo -e "\nAll systems started."
 top
