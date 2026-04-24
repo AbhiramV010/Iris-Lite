ccf9599e2d9fe6899bc71ae2d1cf57c76e001d24
Abhiram <abhiram.vadali@gmail.com>
2026-04-23 21:41:24 -0400

    Iris_lite.sh changed

--- IRIS_Lite.sh
+++ IRIS_Lite.sh
@@ -13,19 +13,19 @@ draw_bar() { # simple yet cool loading bar!
 echo "Starting Iris Lite"
 draw_bar 0
 
-uxterm -T "Camera Utility" -e "python3 ./clips/startCamera.py" & 
+uxterm -T "Camera Utility" -e "python3 ./Decision/startCamera.py" & 
 sleep 1 && draw_bar 1
 
-uxterm -T "Main Utility" -e "python3 ./clips/main.py" & 
+uxterm -T "Main Utility" -e "python3 ./Decision/main.py" & 
 sleep 1 && draw_bar 2
 
-uxterm -T "Detector Pipeline" -e "python3 ./clips/detector_pipeline.py" & 
+uxterm -T "Detector Pipeline" -e "python3 ./Decision/detector_pipeline.py" & 
 sleep 1 && draw_bar 3
 
-uxterm -T "Listener Tool" -e "python3 ./clips/listener.py" & 
+uxterm -T "Listener Tool" -e "python3 ./Decision/listener.py" & 
 sleep 1 && draw_bar 4
 
-uxterm -T "Zone Monitor" -e "python3 ./clips/zone_monitor.py" & 
+uxterm -T "Zone Monitor" -e "python3 ./Decision/zone_monitor.py" & 
 sleep 1 && draw_bar 5
 
 uxterm -T "Compression System" -e "sh -c 'g++ ./Compression/src/main.cpp -o ./Compression/src/compressor && ./Compression/src/compressor; exec bash'" &
