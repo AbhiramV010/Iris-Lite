8068b5b7b2cf142dad9a84e1b1ec9961c09513d1
Abhiram <abhiram.vadali@gmail.com>
2026-04-25 15:01:35 -0400

    fixed IRIS_Lite.sh

--- IRIS_Lite.sh
+++ IRIS_Lite.sh
@@ -13,11 +13,13 @@ draw_bar() { # simple yet cool loading bar!
 echo "Starting Iris Lite"
 draw_bar 0
 
+
+python3 ./Decision/main.py
+
+uxterm -T "TOP" -e "top"
+
 uxterm -T "Camera Utility" -e "python3 ./Decision/startCamera.py" & 
 sleep 2 && draw_bar 1
-
-uxterm -T "Main Utility" -e "python3 ./Decision/main.py" & 
-sleep 2 && draw_bar 2
 
 uxterm -T "Detector Pipeline" -e "python3 ./Decision/detector_pipeline.py" & 
 sleep 2 && draw_bar 3
@@ -32,4 +34,3 @@ uxterm -T "Compression System" -e "bash -c 'cd ~Iris-Lite/Compression && ./build
 sleep 2 && draw_bar 6
 
 echo -e "\nAll systems started."
-top
