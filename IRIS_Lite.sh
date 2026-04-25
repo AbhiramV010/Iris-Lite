81824d636bf010bfd627b33eaa589dc3e1df1fbb
Abhiram <abhiram.vadali@gmail.com>
2026-04-25 15:03:15 -0400

    fixed IRIS_Lite.sh

--- IRIS_Lite.sh
+++ IRIS_Lite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-draw_bar() { # simple yet cool loading bar!
+draw_bar() {
     local percent=$(( $1 * 100 / 6 ))
     local filled=$(( $1 * 2 ))
     local empty=$(( 12 - filled ))
@@ -13,22 +13,24 @@ draw_bar() { # simple yet cool loading bar!
 echo "Starting Iris Lite"
 draw_bar 0
 
-
-uxterm -T "TOP" -e "top"
+# Fixed: Added '&' to prevent blocking
+uxterm -T "TOP" -e "top" & 
+sleep 1 && draw_bar 1
 
 uxterm -T "Camera Utility" -e "python3 ./Decision/startCamera.py" & 
-sleep 2 && draw_bar 1
+sleep 1 && draw_bar 2
 
 uxterm -T "Detector Pipeline" -e "python3 ./Decision/detector_pipeline.py" & 
-sleep 2 && draw_bar 3
+sleep 1 && draw_bar 3
 
 uxterm -T "Listener Tool" -e "python3 ./Decision/listener.py" & 
-sleep 2 && draw_bar 4
+sleep 1 && draw_bar 4
 
 uxterm -T "Zone Monitor" -e "python3 ./Decision/zone_monitor.py" & 
-sleep 2 && draw_bar 5
+sleep 1 && draw_bar 5
 
-uxterm -T "Compression System" -e "bash -c 'cd ~Iris-Lite/Compression && ./build/PerceptualCompressor; exec bash'" &
-sleep 2 && draw_bar 6
+# Fixed: Ensuring path resolution
+uxterm -T "Compression System" -e "bash -c 'cd $HOME/Iris-Lite/Compression && ./build/PerceptualCompressor; exec bash'" &
+sleep 1 && draw_bar 6
 
 echo -e "\nAll systems started."
