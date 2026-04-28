8d58d4b7564b20d30f45a7b24ce1416f0187f44b
valorantmrvolatile-tech <shreyasht2810@gmail.com>
2026-04-28 14:59:59 -0400

    chanes pt2

--- Compression/include/ImportanceSignal.hpp
+++ Compression/include/ImportanceSignal.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
+#include <cstdint>
+
 struct ImportanceSignal
 {
     float motion = 0.0f;
     float spatial = 0.0f;
-    float face = 0.0f;
-    float region = 0.0f;
-
+    float temporal = 0.0f;
     float score = 0.0f;
 };
